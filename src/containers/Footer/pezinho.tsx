6f04d3316a83285a99bb8c759ec5ddb6388f23c1
Cristhyan Couto <cristhyancouto@gmail.com>
2024-09-04 10:15:06 -0300

    isso ai

--- src/containers/Footer/pezinho.tsx
+++ src/containers/Footer/pezinho.tsx
@@ -3,7 +3,7 @@ import "./pezinho.css"
 export const Pezinho = () => {
     return (
         <footer>
-            <p>Feito com ❤️ por <a href="https://www.linkedin.com/in/cristhyan-couto-6ba8581b2/">Cristhyan Couto</a>, Ta bonito? Não, mas funfa</p>
+            <p>Perdeu pato</p>
         </footer>
     )
 }
